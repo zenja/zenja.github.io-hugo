bd4474c1e671eb49efd52b4cf5aa20de94fd1d94
Xing, Wang <xwang16@ebay.com>
2015-11-16 22:26:59 +0800

    fix deploy.sh and make a new test

--- content/post/first.md
+++ content/post/first.md
@@ -1,6 +1,6 @@
 +++
 date = "2015-11-14T15:55:20+08:00"
-draft = true
+draft = false
 title = "first"
 
 +++
@@ -8,3 +8,5 @@ title = "first"
 # This is a first blog for testing
 
 Just for testing...
+
+And let me make a simple change here for another test.
--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,9 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
 # if using a theme, replace by `hugo -t <yourtheme>`
+echo "Generating website..."
 hugo
+echo "Website generated."
 
 # Go To Public folder
 cd public
@@ -18,8 +20,18 @@ if [ $# -eq 1 ]
 fi
 git commit -m "$msg"
 
-# Push source and build repos.
+head_hash=$(git rev-parse HEAD)
+echo "new commit made: ${head_hash}"
+
+# Merge master branch with the change and push to remote
+echo "Checking out master..."
+git checkout master
+echo "merging with ${head_hash}..."
+git merge ${head_hash}
+echo "pushing master to remote"
 git push origin master
 
 # Come Back
 cd ..
+
+echo "Done."
